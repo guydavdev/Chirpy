67eaf47be06e56b71686d87f62d84cbf47217eec
guy.davidson <ind.guy@icloud.com>
2025-08-19 15:42:28 +0300

    with healthz

--- main.go
+++ main.go
@@ -7,9 +7,19 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+
+	fileServerPrefix := "/app/"
+	fileServerMux := http.StripPrefix(fileServerPrefix, http.FileServer(http.Dir(".")))
+	mux.Handle(fileServerPrefix, fileServerMux)
+	mux.HandleFunc("GET /healthz", handleReadiness)
 
 	fmt.Println("Starting server...")
 	server := http.Server{Handler: mux, Addr: ":8080"}
 	server.ListenAndServe()
+}
+
+func handleReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
 }
