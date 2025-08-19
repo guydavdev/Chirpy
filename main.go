77e0408884f0872d74f1def653044ca90aca8ebd
guy.davidson <ind.guy@icloud.com>
2025-08-19 13:32:50 +0300

    with file server

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(".")))
 
 	fmt.Println("Starting server...")
 	server := http.Server{Handler: mux, Addr: ":8080"}
