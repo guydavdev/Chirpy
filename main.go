60dd43d8fb3b06d7d6cb3e7d61cb28fa69daea63
guy.davidson <ind.guy@icloud.com>
2025-08-22 09:49:48 +0300

    with file structure

--- main.go
+++ main.go
@@ -1,51 +1,32 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net/http"
 	"sync/atomic"
 )
 
-func main() {
-	apiConfig := apiConfig{}
-	mux := http.NewServeMux()
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
 
+func main() {
+	const filepathRoot = "."
+	const port = "8080"
+	apiConfig := apiConfig{}
+
+	mux := http.NewServeMux()
 	fileServerPrefix := "/app/"
 	fileServerMux := http.StripPrefix(fileServerPrefix, http.FileServer(http.Dir(".")))
 	mux.Handle(fileServerPrefix, apiConfig.middlewareMetricsInc(fileServerMux))
-	mux.HandleFunc("GET /healthz", handleReadiness)
-	mux.HandleFunc("GET /metrics", apiConfig.handleMetrics)
-	mux.HandleFunc("POST /reset", apiConfig.handleReset)
 
-	fmt.Println("Starting server...")
-	server := http.Server{Handler: mux, Addr: ":8080"}
-	server.ListenAndServe()
-}
+	mux.HandleFunc("GET /api/healthz", handleReadiness)
 
-func handleReadiness(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("OK"))
-}
+	mux.HandleFunc("GET /admin/metrics", apiConfig.handleMetrics)
+	mux.HandleFunc("POST /admin/reset", apiConfig.handleReset)
 
-func (cfg *apiConfig) handleMetrics(w http.ResponseWriter, r *http.Request) {
-	hits := fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(hits))
-}
+	server := http.Server{Handler: mux, Addr: ":" + port}
 
-func (cfg *apiConfig) handleReset(w http.ResponseWriter, r *http.Request) {
-	cfg.fileserverHits.Store(0)
-	w.WriteHeader(http.StatusOK)
-}
-
-func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		cfg.fileserverHits.Add(1)
-		next.ServeHTTP(w, r)
-	})
-}
-
-type apiConfig struct {
-	fileserverHits atomic.Int32
+	log.Printf("Serving files from %s on port %s\n", filepathRoot, port)
+	log.Fatal(server.ListenAndServe())
 }
