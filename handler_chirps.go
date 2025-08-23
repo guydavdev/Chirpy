a3726c4513548ec0b02e64078d3e1aacda44986a
guy.davidson <ind.guy@icloud.com>
2025-08-23 09:05:55 +0300

    with full handler chirps

--- handler_chirps.go
+++ handler_chirps.go
@@ -3,16 +3,25 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/guydavdev/Chirpy/internal/database"
 )
 
-func (cfg apiConfig) handlerChirps(w http.ResponseWriter, r *http.Request) {
+type Chirp struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Body      string    `json:"body"`
+	UserID    uuid.UUID `json:"user_id"`
+}
+
+func (cfg *apiConfig) handlerChirps(w http.ResponseWriter, r *http.Request) {
 	const maxChirpLength = 140
 	type parameters struct {
 		Body   string `json:"body"`
 		UserID string `json:"user_id"`
-	}
-	type returnVals struct {
-		Cleaned string `json:"cleaned_body"`
 	}
 
 	decoder := json.NewDecoder(r.Body)
@@ -27,12 +36,26 @@ func (cfg apiConfig) handlerChirps(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if params.UserID == "" {
-		respondWithError(w, http.StatusBadRequest, "UserId must be set", nil)
+	userUUID, err := uuid.Parse(params.UserID)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid user_id", nil)
 		return
 	}
 
-	respondWithJSON(w, http.StatusOK, returnVals{
-		Cleaned: cleanBody(params.Body),
+	chirp, err := cfg.db.CreateChirp(r.Context(), database.CreateChirpParams{
+		Body:   cleanBody(params.Body),
+		UserID: userUUID,
+	})
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Failed to create chirp:", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusCreated, Chirp{
+		ID:        chirp.ID,
+		CreatedAt: chirp.CreatedAt,
+		UpdatedAt: chirp.UpdatedAt,
+		Body:      chirp.Body,
+		UserID:    chirp.UserID,
 	})
 }
