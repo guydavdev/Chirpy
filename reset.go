416c99c63deb38209ae0f0c999a4686a8122b816
guy.davidson <ind.guy@icloud.com>
2025-08-23 08:13:13 +0300

    minors

--- reset.go
+++ reset.go
@@ -15,4 +15,5 @@ func (cfg *apiConfig) handleReset(w http.ResponseWriter, r *http.Request) {
 
 	cfg.fileserverHits.Store(0)
 	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Hits reset to 0 and database reset to initial state."))
 }
