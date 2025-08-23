e42eaaaf144899b487f3ee3f5e8bd1e26a9df6c4
guy.davidson <ind.guy@icloud.com>
2025-08-23 08:52:02 +0300

    with generated CreateChirp

--- sql/schema/002_chirps.sql
+++ sql/schema/002_chirps.sql
@@ -4,7 +4,7 @@ CREATE TABLE chirps (
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
     body TEXT NOT NULL,
-    user_id UUID,
+    user_id UUID NOT NULL,
     CONSTRAINT fk_user_id
     FOREIGN KEY (user_id)
     REFERENCES users(id) ON DELETE CASCADE
