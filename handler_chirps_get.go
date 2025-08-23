dcd654f470c38d87657bf5ba5c3490055b368767
guy.davidson <ind.guy@icloud.com>
2025-08-23 10:22:31 +0300

    with genrated get chirps query

--- handler_chirps_get.go
+++ handler_chirps_get.go
@@ -3,5 +3,22 @@ package main
 import "net/http"
 
 func (cfg *apiConfig) handlerGetChirps(w http.ResponseWriter, r *http.Request) {
+	chirpsData, err := cfg.db.GetChirps(r.Context())
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Failed to get chirps", err)
+		return
+	}
 
+	chirps := make([]Chirp, len(chirpsData))
+	for i, chirp := range chirpsData {
+		chirps[i] = Chirp{
+			ID:        chirp.ID,
+			CreatedAt: chirp.CreatedAt,
+			UpdatedAt: chirp.UpdatedAt,
+			Body:      chirp.Body,
+			UserID:    chirp.UserID,
+		}
+	}
+
+	respondWithJSON(w, http.StatusOK, chirps)
 }
