5d2f3454757f22724ba259e64a13d0cb4bc9b3f2
guy.davidson <ind.guy@icloud.com>
2025-08-23 10:50:44 +0300

    with GET /api/chirps/{chirpID}

--- handler_chirp_get.go
+++ handler_chirp_get.go
@@ -1,7 +1,42 @@
 package main
 
-import "net/http"
+import (
+	"database/sql"
+	"errors"
+	"net/http"
+
+	"github.com/google/uuid"
+)
 
 func (cfg *apiConfig) handlerGetChirp(w http.ResponseWriter, r *http.Request) {
+	chirpID := r.PathValue("chirpID")
 
+	if chirpID == "" {
+		respondWithError(w, http.StatusBadRequest, "Invalid chirpID", nil)
+		return
+	}
+
+	chirpUUID, err := uuid.Parse(chirpID)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid chirpID", nil)
+		return
+	}
+
+	chirp, err := cfg.db.GetChirp(r.Context(), chirpUUID)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			respondWithError(w, http.StatusNotFound, "Chirp not found", err)
+			return
+		}
+		respondWithError(w, http.StatusInternalServerError, "Failed to get Chirp", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, Chirp{
+		ID:        chirp.ID,
+		CreatedAt: chirp.CreatedAt,
+		UpdatedAt: chirp.UpdatedAt,
+		Body:      chirp.Body,
+		UserID:    chirp.UserID,
+	})
 }
